ae58549634caef0addf0bd434d74a45aec75d58e
Graham Goldstein <g.goldstein@live.com>
2018-12-03 08:42:47 +0000

    Format dates as strings, pass limit and offset parameters correctly

--- application_planning_database.py
+++ application_planning_database.py
@@ -8,12 +8,28 @@ def cursor():
 	return conn.cursor()
 
 def applications_by_validated_date(limit=25, offset=0, search=''):
+	if limit == None:
+		limit = 25
+	if offset == None:
+		offset = 0
+	if search == None:
+		search = ''
 	c = cursor()
 	c.execute(
 		"""
-			SELECT reference, title, link, address, received_date, validated_date, status
+			SELECT reference, title, link, address, TO_CHAR(received_date :: DATE, 'dd/mm/yyyy'), TO_CHAR(validated_date :: DATE, 'dd/mm/yyyy'), status
 			FROM application
-			ORDER BY validated_date DESC
+			ORDER BY validated_date DESC, received_date DESC, reference DESC
 			LIMIT %s OFFSET %s
 		""", [limit, offset])
-	yield from c.fetchmany(limit)
+	for row in c.fetchmany(limit):
+		application = {
+			'reference': row[0],
+			'title': row[1],
+			'link': row[2],
+			'address': row[3],
+			'received_date': row[4],
+			'validated_date': row[5],
+			'status': row[6]
+		}
+		yield application
