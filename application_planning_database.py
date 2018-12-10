1fe40e8722b622e3fc605de6021da307f4cca990
Graham Goldstein <g.goldstein@live.com>
2018-12-10 20:09:07 +0000

    More integerising for limit parameter

--- application_planning_database.py
+++ application_planning_database.py
@@ -22,7 +22,7 @@ def applications_by_validated_date(limit=25, offset=0, search=''):
 			ORDER BY validated_date DESC, received_date DESC, reference DESC
 			LIMIT %s OFFSET %s
 		""", [int(limit), int(offset)])
-	for row in c.fetchmany(limit):
+	for row in c.fetchmany(int(limit)):
 		application = {
 			'reference': row[0],
 			'title': row[1],
